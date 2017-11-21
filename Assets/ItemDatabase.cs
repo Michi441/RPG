0dccb145c375b4bc12bcbe7357cf0e0786a5290c
Doris <DorisMac@Doriss-MBP.local>
2017-11-21 15:30:50 +0100

    Added UI for Quest

---
+++ Assets/ItemDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase {
+
+	public static Dictionary<int, string> items = new Dictionary<int, string> () {
+
+		{ 0, "apple" },
+		{ 1, "banana" },
+		{ 2, "paper" }
+	};
+}
---
+++ Assets/NPCDataBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDataBase{
+
+	public static Dictionary<int, string> npcs = new Dictionary<int, string>(){
+
+		{0, "Witch Doctor"},
+		{1, "Nurse"},
+		{3, "Whatever"}
+	};
+}
